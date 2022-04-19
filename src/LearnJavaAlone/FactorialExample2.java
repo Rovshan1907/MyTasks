4e3f5ce330227f6fa962a91a8736e9e66f692b9a
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-04-19 21:56:27 +0200

    let's try the number to calcuate factorial

--- src/LearnJavaAlone/FactorialExample2.java
+++ src/LearnJavaAlone/FactorialExample2.java
@@ -2,6 +2,15 @@ package LearnJavaAlone;
 
 public class FactorialExample2 {
     // Factorial Program using recursion
+    static int factorial(int n){
+        if (n ==0)
+            return 1;
+     else
+         return(n*factorial(n-1));
+    }
+/// till here static method we wrote let's the find number to calculate
+    public static void main(String[] args) {
 
+    }
 
 }
