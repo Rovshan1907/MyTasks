e6d77bd41a8fd3fac151960c983d992f7a1568a3
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-04-19 21:47:16 +0200

    Factorial Program using Loop

--- src/LearnJavaAlone/FactorialExample.java
+++ src/LearnJavaAlone/FactorialExample.java
@@ -5,8 +5,11 @@ public class FactorialExample {
 
         int i,fact=1;
         int number =5; // It is the number to calculate factorial
+        for(i=1;i<number;i++){
+            fact=fact*i; // here is factoriial
+        }
 
-
+        System.out.println("Factorial of"+number+"is:"+fact); //here will give us output factorial of 5 is 120
 
 
 
