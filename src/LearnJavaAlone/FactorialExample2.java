49978319482bcd2c52958920338ed4503c2e2176
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-04-19 21:59:51 +0200

    Done Test working

--- src/LearnJavaAlone/FactorialExample2.java
+++ src/LearnJavaAlone/FactorialExample2.java
@@ -10,6 +10,11 @@ public class FactorialExample2 {
     }
 /// till here static method we wrote let's the find number to calculate
     public static void main(String[] args) {
+        int i,fact=1;
+        int number = 4;
+        // it is the number to calculate factorial
+        fact = factorial(number);
+        System.out.println("Factorial of"+number+"is:"+fact);
 
     }
 
