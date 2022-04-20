72f9fbaa18ac4286aa756320d361a999df2a0627
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-04-20 11:27:30 +0200

    And now let's write the code

--- src/LearnJavaAlone/ArmStrongNumber.java
+++ src/LearnJavaAlone/ArmStrongNumber.java
@@ -1,4 +1,16 @@
 package LearnJavaAlone;
 
 public class ArmStrongNumber {
+
 }
+/*
+Armstrong Number is a positive number if it is equal to the
+ sum of cubes of its digits for example 0,1,153,370,371,407 and more....
+ Let's try to underrtsnad why 153 is an Amrstrong number
+152 = (1*1*1)+(5*5*5)+(3*3*3)
+where:
+1
+125
+27
+and 1+125+27=153
+ */
