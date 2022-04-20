8b8f80a51dd2dbb216e42c27a154f757f294a5b7
Rovshan1907 <97406408+Rovshan1907@users.noreply.github.com>
2022-04-20 11:34:36 +0200

    An Armstrong Number

--- src/LearnJavaAlone/ArmStrongNumber.java
+++ src/LearnJavaAlone/ArmStrongNumber.java
@@ -1,6 +1,21 @@
 package LearnJavaAlone;
 
 public class ArmStrongNumber {
+    public static void main(String[] args) {
+        int c=0,a,temp;
+        int n =153;
+      //  it is the number to check armstrong
+      temp=n;
+      while(n>0){
+          a=n%10;
+          n=n/10;
+          c=c+(a*a*a);
+      }
+      if (temp==c)
+          System.out.println("An Armstrong Number");
+      else
+          System.out.println("Not An Armstrong Number");
+    }
 
 }
 /*
